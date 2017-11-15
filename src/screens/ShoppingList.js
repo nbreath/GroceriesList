a8c9b8f901b7ab77d0353f20224aa27620983224
John <john@kommit.co>
2017-11-14 23:15:21 -0500

    Add listeners to shopping list

--- src/screens/ShoppingList.js
+++ src/screens/ShoppingList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Alert } from 'react-native-prompt-android';
+import { Alert } from 'react-native';
 import {
   Body,
   Container,
@@ -25,6 +25,40 @@ export default class ShoppingList extends Component {
     };
   }
 
+  handleProductPress(product) {
+    this.state.products.forEach(p => {
+      if (product.id === p.id) {
+        p.gotten = !p.gotten;
+      }
+      return p;
+    });
+
+    this.setState({ products: this.state.products });
+  }
+
+  handleAddProductPress() {
+    this.props.navigation.navigate('AddProduct', {
+      addProduct: product => {
+        this.setState({
+          products: this.state.products.concat(product)
+        });
+      },
+      deleteProduct: product => {
+        this.setState({
+          products: this.state.products.filter(p => p.id !== product.id)
+        });
+      },
+      productsInList: this.state.products
+    });
+  }
+
+  handleClearProductPress() {
+    Alert.alert('Clear all items?', null, [
+      { text: 'Cancel' },
+      { text: 'Ok', onPress: () => this.setState({ products: [] }) }
+    ]);
+  }
+
   render() {
     return (
       <Container>
@@ -33,12 +67,18 @@ export default class ShoppingList extends Component {
             {
               this.state.products.map(p => {
                 return (
-                  <ListItem key={p.id}>
+                  <ListItem
+                    key={p.id}
+                    onPress={this.handleProductPress.bind(this, p)}
+                  >
                     <Body>
                       <Text>{p.name}</Text>
                     </Body>
                     <Right>
-                      <CheckBox checked={false} />
+                      <CheckBox
+                        checked={false}
+                        onPress={this.handleProductPress.bind(this, p)}
+                      />
                     </Right>
                   </ListItem>
                 );
@@ -46,10 +86,18 @@ export default class ShoppingList extends Component {
             }
           </List>
         </Content>
-        <Fab style={{ backgroundColor: '#5067FF' }} position="bottomRight">
+        <Fab
+          style={{ backgroundColor: '#5067FF' }}
+          position="bottomRight"
+          onPress={this.handleAddProductPress.bind(this)}
+        >
           <Icon name="add" />
         </Fab>
-        <Fab style={{ backgroundColor: 'red' }} position="bottomLeft">
+        <Fab
+          style={{ backgroundColor: 'red' }}
+          position="bottomLeft"
+          onPress={this.handleClearProductPress.bind(this)}
+        >
           <Icon ios="ios-remove" android="md-remove" />
         </Fab>
       </Container>
